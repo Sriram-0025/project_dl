e46502b142363274c549200e0399231f0ace6334
Sriram-0025 <117933220+Sriram-0025@users.noreply.github.com>
2024-08-28 23:07:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,16 +30,16 @@ st.title('Dermatology Classification')
 st.write("Enter the feature values for prediction:")
 
 # Inputs
-thinning = st.number_input('Thinning of the suprapapillary epidermis', min_value=0, max_value=10)
-clubbing = st.number_input('Clubbing of the rete ridges', min_value=0, max_value=10)
-spongiosis = st.number_input('Spongiosis', min_value=0, max_value=10)
-fibrosis = st.number_input('Fibrosis of the papillary dermis', min_value=0, max_value=10)
-koebner = st.number_input('Koebner phenomenon', min_value=0, max_value=10)
-elongation = st.number_input('Elongation of the rete ridges', min_value=0, max_value=10)
-exocytosis = st.number_input('Exocytosis', min_value=0, max_value=10)
-melanin = st.number_input('Melanin incontinence', min_value=0, max_value=10)
-pnl_infiltrate = st.number_input('Pnl infiltrate', min_value=0, max_value=10)
-saw_tooth = st.number_input('Saw-tooth appearance of retes', min_value=0, max_value=10)
+thinning = st.number_input('Thinning of the suprapapillary epidermis', min_value=0, max_value=3)
+clubbing = st.number_input('Clubbing of the rete ridges', min_value=0, max_value=3)
+spongiosis = st.number_input('Spongiosis', min_value=0, max_value=3)
+fibrosis = st.number_input('Fibrosis of the papillary dermis', min_value=0, max_value=3)
+koebner = st.number_input('Koebner phenomenon', min_value=0, max_value=3)
+elongation = st.number_input('Elongation of the rete ridges', min_value=0, max_value=3)
+exocytosis = st.number_input('Exocytosis', min_value=0, max_value=3)
+melanin = st.number_input('Melanin incontinence', min_value=0, max_value=3)
+pnl_infiltrate = st.number_input('Pnl infiltrate', min_value=0, max_value=3)
+saw_tooth = st.number_input('Saw-tooth appearance of retes', min_value=0, max_value=3)
 
 # Collect input data
 new_input_data = np.array([[thinning, clubbing, spongiosis, fibrosis, koebner,
